c29a4291381e9f71d9cd0684888fc0420bcaad65
Jeremy Pichon <jeremy.pichon@fr.ibm.com>
2017-10-23 00:45:08 -0700

    Externalize ICP CV login url to travis as private variable.

--- get-icp-cv.sh
+++ get-icp-cv.sh
@@ -3,7 +3,7 @@
 
 if [ ! -f $HOME/.cache/$ICP_CV_FILE_NAME ]; then
     echo "ICP Content Verification: Starting download..."
-    AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
+    AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" $ICP_CV_LOGIN_URL | grep -E "X-Auth-Token:" | awk {'print $2'})
     curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ICP_CV_URL
     mv $ICP_CV_FILE_NAME $HOME/.cache/
     cd $HOME/.cache
