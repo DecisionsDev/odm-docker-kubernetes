c7353e306c59ca84c5b7daa57cda6c96bca72002
Pierre-Yves Lochou <pylochou@fr.ibm.com>
2022-01-26 17:40:27 +0100

    Accept env vars as input for OKTA_*

--- authentication/Okta/generateTemplate.sh
+++ authentication/Okta/generateTemplate.sh
@@ -4,41 +4,52 @@ export OKTA_CLAIM_GROUPS="groups"
 export OKTA_CLAIM_LOGIN="loginName"
 OUTPUT_DIR=./output
 TEMPLATE_DIR=./templates
+
 function usage {
-    print "$0 Available options:"
-    print " -x : Cient Secret"
-    print " -i : Client ID"
-    print " -s : Okta Server URL"
-    print " -g : Okta ODM Group"
-    print " "
-    print "Usage example : $0 -i OktaclientId -x OktaclientSecret -s https://fr-ibmodmdev.okta.com -g odm-admin"
+  cat <<EOF
+Usage: $(basename "$0") [-<option letter> <option value>] [-h]
+
+Options:
+
+-g : Okta ODM Group"
+-i : Client ID"
+-s : Okta Server URL"
+-x : Cient Secret"
+
+Usage example: $0 -i OktaclientId -x OktaclientSecret -s https://fr-ibmodmdev.okta.com -g odm-admin"
+EOF
 }
-while getopts "x:i:s:g:h" option 
-do
+
+while getopts "x:i:s:g:h" option; do
     case "${option}" in
-        x)
-            OKTA_CLIENT_SECRET=${OPTARG}
-            ;;
-        i)
-            OKTA_CLIENT_ID=${OPTARG}
-            ;;
-        s)
-            OKTA_SERVER_URL=${OPTARG}
-            ;;
-        g)
-            OKTA_ODM_GROUP=${OPTARG}
-            ;;
-        h)
-            usage
-            exit 0
-            ;;
-        *)
-            usage
-            exit 1
-            ;;
+        g) OKTA_ODM_GROUP=${OPTARG};;
+        i) OKTA_CLIENT_ID=${OPTARG};;
+        s) OKTA_SERVER_URL=${OPTARG};;
+        x) OKTA_CLIENT_SECRET=${OPTARG};;
+        h) usage; exit 0;;
+        *) usage; exit 1;;
     esac
 done
 
+if [[ -z ${OKTA_ODM_GROUP} ]]; then
+  echo "OKTA_ODM_GROUP has to be provided, either as in environment or with -g."
+  exit 1
+fi
+if [[ -z ${OKTA_CLIENT_ID} ]]; then
+  echo "OKTA_CLIENT_ID has to be provided, either as in environment or with -i."
+  exit 1
+fi
+if [[ -z ${OKTA_SERVER_URL} ]]; then
+  echo "OKTA_SERVER_URL has to be provided, either as in environment or with -s."
+  exit 1
+fi
+if [[ -z ${OKTA_CLIENT_SECRET} ]]; then
+  echo "OKTA_CLIENT_SECRET has to be provided, either as in environment or with -x."
+  exit 1
+fi
+if [[ ${OKTA_SERVER_URL} != "https://.*" ]]; then
+  OKTA_SERVER_URL=https://${OKTA_SERVER_URL}
+fi
 
 mkdir -p $OUTPUT_DIR && cp $TEMPLATE_DIR/* $OUTPUT_DIR
 echo "Generating files for Okta"
