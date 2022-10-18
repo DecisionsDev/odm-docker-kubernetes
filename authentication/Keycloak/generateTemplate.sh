5eb3dd551aeeea11729844c74f7c147b12dc9840
mathias-mouly <mathias.mouly@fr.ibm.com>
2022-10-18 17:49:47 +0200

    script and template

--- authentication/Keycloak/generateTemplate.sh
+++ authentication/Keycloak/generateTemplate.sh
@@ -15,8 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-export AZUREAD_CLAIM_GROUPS="groups"
-export AZUREAD_CLAIM_LOGIN="loginName"
+export KEYCLOAK_USERID="preferred_username"
 OUTPUT_DIR=./output
 TEMPLATE_DIR=./templates
 
@@ -26,60 +25,54 @@ Usage: $(basename "$0") [-<option letter> <option value>] [-h]
 
 Options:
 
--g : AZUREAD ODM Group ID
 -i : Client ID
--n : AZUREAD domain (AZUREAD server name)
+-n : KEYCLOAK URL (KEYCLOAK server name)
 -x : Cient Secret
+-g : ODM Admin Group
 -a : Allow others domains (Optional)
-Usage example: $0 -i AzureADClientId -x AzureADClientSecret -n <Application ID (GUID)> -g <GROUP ID (GUID)> [-a <domain name>]"
+Usage example: $0 -i KeycloakClientId -x KeycloakClientSecret [-g <ODM Admin Group> -a <domain name>]"
 EOF
 }
 
-while getopts "x:i:n:s:g:ha:" option; do
+while getopts "x:i:n:g:ha:" option; do
     case "${option}" in
-        g) AZUREAD_ODM_GROUP_ID=${OPTARG};;
-        i) AZUREAD_CLIENT_ID=${OPTARG};;
-        n) AZUREAD_SERVER_NAME=${OPTARG};;
-        x) AZUREAD_CLIENT_SECRET=${OPTARG};;
+        i) KEYCLOAK_CLIENT_ID=${OPTARG};;
+        n) KEYCLOAK_URL=${OPTARG};;
+        x) KEYCLOAK_CLIENT_SECRET=${OPTARG};;
+        g) KEYCLOAK_ADMIN_GROUP=${OPTARG};;
         a) ALLOW_DOMAIN=${OPTARG};;
         h) usage; exit 0;;
         *) usage; exit 1;;
     esac
 done
 
-if [[ -z ${AZUREAD_ODM_GROUP_ID} ]]; then
-  echo "AZUREAD_ODM_GROUP_ID has to be provided, either as in environment or with -g."
+if [[ -z ${KEYCLOAK_CLIENT_ID} ]]; then
+  echo "KEYCLOAK_CLIENT_ID has to be provided, either as in environment or with -i."
   exit 1
 fi
-if [[ -z ${AZUREAD_CLIENT_ID} ]]; then
-  echo "AZUREAD_CLIENT_ID has to be provided, either as in environment or with -i."
+if [[ -z ${KEYCLOAK_URL} ]]; then
+  echo "KEYCLOAK_URL has to be provided, either as in environment or with -n."
   exit 1
 fi
-if [[ -z ${AZUREAD_SERVER_NAME} ]]; then
-  echo "AZUREAD_SERVER_NAME has to be provided, either as in environment or with -n."
+if [[ -z ${KEYCLOAK_CLIENT_SECRET} ]]; then
+  echo "KEYCLOAK_CLIENT_SECRET has to be provided, either as in environment or with -x."
   exit 1
-fi
-if [[ -z ${AZUREAD_CLIENT_SECRET} ]]; then
-  echo "AZUREAD_CLIENT_SECRET has to be provided, either as in environment or with -x."
-  exit 1
-fi
-
-if [[ ${AZUREAD_SERVER_NAME} != "https://.*" ]]; then
-  AZUREAD_SERVER_URL=https://login.microsoftonline.com/${AZUREAD_SERVER_NAME}
-else
-  AZUREAD_SERVER_URL=${AZUREAD_SERVER_NAME}
 fi
 
 mkdir -p $OUTPUT_DIR && cp $TEMPLATE_DIR/* $OUTPUT_DIR
-echo "Generating files for AZUREAD"
-sed -i.bak 's|AZUREAD_CLIENT_ID|'$AZUREAD_CLIENT_ID'|g' $OUTPUT_DIR/*
-sed -i.bak 's|AZUREAD_CLIENT_SECRET|'$AZUREAD_CLIENT_SECRET'|g' $OUTPUT_DIR/*
-sed -i.bak 's|AZUREAD_ODM_GROUP_ID|'$AZUREAD_ODM_GROUP_ID'|g' $OUTPUT_DIR/*
-sed -i.bak 's|AZUREAD_SERVER_URL|'$AZUREAD_SERVER_URL'|g' $OUTPUT_DIR/*
+echo "Generating files for KEYCLOAK"
+sed -i.bak 's|KEYCLOAK_CLIENT_ID|'$KEYCLOAK_CLIENT_ID'|g' $OUTPUT_DIR/*
+sed -i.bak 's|KEYCLOAK_CLIENT_SECRET|'$KEYCLOAK_CLIENT_SECRET'|g' $OUTPUT_DIR/*
+if [ ! -z "$KEYCLOAK_ADMIN_GROUP" ]; then
+  sed -i.bak 's|KEYCLOAK_ADMIN_GROUP|'$KEYCLOAK_ADMIN_GROUP'|g' $OUTPUT_DIR/*
+else
+sed -i.bak 's|KEYCLOAK_ADMIN_GROUP|odm-admin|g' $OUTPUT_DIR/*
+fi
+sed -i.bak 's|KEYCLOAK_URL|'$KEYCLOAK_URL'|g' $OUTPUT_DIR/*
 # Claim replacement
-sed -i.bak 's|AZUREAD_CLAIM_GROUPS|'$AZUREAD_CLAIM_GROUPS'|g' $OUTPUT_DIR/*
-sed -i.bak 's|AZUREAD_CLAIM_LOGIN|'$AZUREAD_CLAIM_LOGIN'|g' $OUTPUT_DIR/*
 if [ ! -z "$ALLOW_DOMAIN" ]; then
-  sed -i.bak 's|login.microsoftonline.com|'login.microsoftonline.com,$ALLOW_DOMAIN'|g' $OUTPUT_DIR/*
+  sed -i.bak 's|KEYCLOAK_DOMAIN|'$ALLOW_DOMAIN'|g' $OUTPUT_DIR/*
+else
+
 fi
 rm -f $OUTPUT_DIR/*.bak
