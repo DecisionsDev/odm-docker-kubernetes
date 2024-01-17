14b8140c32dc265897dd08e21c5b7bf148676191
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2024-01-17 15:12:30 +0100

    Update vault.sh

--- contrib/vault/configmap/vault.sh
+++ contrib/vault/configmap/vault.sh
@@ -5,7 +5,6 @@ configure_decisions() {
   echo "Targeted Environment : $TARGETENV"
 
   case "$CONTAINER_NAME" in
-  "odm.oidc-registration") configure_decisions_registration_job;;
   *) configure_decisions_odm_pod;;
   esac
 
@@ -15,30 +14,11 @@ configure_decisions() {
 configure_decisions_odm_pod() {
   echo "Configuring ODM Pod"
   CONTAINERENVFILE=/ibm/icp4ba/decisions/init/container.env
-  # CONTAINERENVFILE=container.env
-
-  echo "Retrieved Token from custom claim"
-
-  if [ -f "/ibm/icp4ba/pv/token" ]; then
-      echo "Found PV and token file"
-      cat /ibm/icp4ba/pv/token
-  else
-      echo "No PV found."
-      # touch /ibm/icp4ba/pv/token
-  fi
 
   echo "Extract DB information from Vault and put information in /script/init/container.env file"
-
-  echo "LS STORE -----"
-  ls -1 /mnt/secrets-store/*
-  echo "ENDLS"
   echo DB_USER=$(cat /mnt/secrets-store/db-user) >> $CONTAINERENVFILE
   echo DB_PASSWORD=$(cat /mnt/secrets-store/db-password) >> $CONTAINERENVFILE
 
-  # TODO To be docuemted
-#  echo OPENID_CLIENT_ID="odm-oidc-client-id-secret" >> $CONTAINERENVFILE
-#  echo OPENID_CLIENT_SECRET="odm-oidc-client-id-secret-value" >> $CONTAINERENVFILE
-  
   # echo "Inject datasource and driver files"
   # cp /ibm/icp4ba/initconfig/datasource.xml /ibm/icp4ba/decisions/customdatasource/
   # cp /ibm/icp4ba/initconfig/postgresql-42.2.18.jar /ibm/icp4ba/decisions/jdbcdrivers/
@@ -46,30 +26,28 @@ configure_decisions_odm_pod() {
   echo "Inject Private certificate"
   # Get tls.crt and tls.key from Vault and copy it to the directory /ibm/icp4ba/shared/privatecertificates
   # Needed for Client Certificate
-#  cp /mnt/secrets-store/tls.crt /ibm/icp4ba/shared/privatecertificates/tls.crt
-#  cp /mnt/secrets-store/tls.key /ibm/icp4ba/shared/privatecertificates/tls.key
+  #  cp /mnt/secrets-store/tls.crt /ibm/icp4ba/shared/privatecertificates/tls.crt
+  #  cp /mnt/secrets-store/tls.key /ibm/icp4ba/shared/privatecertificates/tls.key
 
   echo "Inject trusted certificate list"
   # Get *.crt trusted certificates from Vault and copy it to the directory /ibm/icp4ba/trusted-cert-volume/
   # For ODM needs to create a sub directory that contains the crt file.
   cp /mnt/secrets-store/digicert.crt /ibm/icp4ba/shared/trustedcertificates/
   cp /mnt/secrets-store/microsoft.crt /ibm/icp4ba/shared/trustedcertificates/
-#  cp /ibm/icp4ba/initconfig/automationcloud.crt /ibm/icp4ba/initconfig/blueworkscloud.crt /ibm/icp4ba/shared/trustedcertificates/
 
   echo "Inject Kafka/BAI configuration file"
   # The sensitive information from this properties files should be retrieved from Vault
   # and replace it. This format is documented in the ODM on K8S documentations. NEeds to restore the documentation for ODM on CP4BA
   # Once the file has been generated needs to copy it to the location /ibm/icp4ba/decisions/baiemitter/
-  #get_iaf_configuration 
-#  cp /ibm/icp4ba/initconfig/plugin-configuration.properties  /ibm/icp4ba/decisions/baiemitter/plugin-configuration.properties
-#  cat /ibm/icp4ba/initconfig/plugin-configuration.properties 
+  #  cp /ibm/icp4ba/initconfig/plugin-configuration.properties  /ibm/icp4ba/decisions/baiemitter/plugin-configuration.properties
+
   echo "Change auth and copy files"
   # To test usecase user access without openid needs to put oidc.enabled -> false
   if [ -f /mnt/secrets-store/OdmOidcProvidersAzureAD.json ]; then echo "Updating OdmOidcProvidersAzureAD.json" && cp /mnt/secrets-store/OdmOidcProvidersAzureAD.json /ibm/icp4ba/decisions/auth/; fi;
   if [ -f /mnt/secrets-store/openIdParameters.properties ]; then echo "Updating openIdParameters.properties" && cp /mnt/secrets-store/openIdParameters.properties /ibm/icp4ba/decisions/auth/; fi;
   if [ -f /mnt/secrets-store/openIdWebSecurity.xml ]; then echo "Updating openIdWebSecurity.xml" && cp /mnt/secrets-store/openIdWebSecurity.xml /ibm/icp4ba/decisions/auth/; fi;
   if [ -f /mnt/secrets-store/webSecurity.xml ]; then echo "Updating owebSecurity.xml" && cp /mnt/secrets-store/webSecurity.xml /ibm/icp4ba/decisions/auth/; fi;
- # cp /ibm/icp4ba/initconfig/simpleWebSecurity.xml /ibm/icp4ba/decisions/auth/webSecurity.xml
+  # cp /ibm/icp4ba/initconfig/simpleWebSecurity.xml /ibm/icp4ba/decisions/auth/webSecurity.xml
 }
 
 
