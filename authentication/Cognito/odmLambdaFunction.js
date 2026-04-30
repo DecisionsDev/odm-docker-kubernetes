fcb0924e080f3a238f1c8ba2414d91c59fdb2ff0
Frederic Mercier <frederic.mercier@gmx.net>
2026-04-30 10:21:39 +0200

    DBACLD-212681 Validate Instructions for using Cognito (#274)
    * DBACLD-212681 Validate Instructions for using Cognito
    
    * update README.md
    
    * fix link from DC to RES
    
    * update detect-secrets
    
    * remove post_logout from README.md as Cognito does not support the post_logout_redirect_uri parameter
    
    ---------
    
    Co-authored-by: Frederic Mercier <f.mercier@fr.ibm.com>

--- authentication/Cognito/odmLambdaFunction.js
+++ authentication/Cognito/odmLambdaFunction.js
@@ -1,31 +1,25 @@
-export const handler = function(event, context) {
+export const handler = async (event, context) => {
   console.debug("enter in ODM lambda");
-  // Allow to get debug information in the Watcher
-  console.debug("context");
-  console.debug(context);
-  
-  console.debug("event");
-  console.debug(event);
-  console.debug("clientId");
-  console.debug(event.callerContext.clientId);
+  console.debug("context=",  context);
+  console.debug("event=",    event);
+  console.debug("clientId=", event.callerContext.clientId);
+  console.debug("userAttributes=", event.request.userAttributes);
 
-  console.debug("userAttributes");
-  console.debug(event.request.userAttributes);
-
-  var identity_for_access_token = event.callerContext.clientId;
+  var identity_for_access_token;
   if (event.request.userAttributes.email != undefined) {
-    console.debug("user email is defined. Use user email as claim identity for the access_token - Rule Designer Context");
-    identity_for_access_token = event.request.userAttributes.email 
+    console.debug("user email is defined. Using user email as claim identity for the access_token - Rule Designer Context");
+    identity_for_access_token = event.request.userAttributes.email
   } else {
-    console.debug("user email is undefined. Use clienId as claim identity for the access_token - M2M Context with client-credentials");
+    console.debug("user email is undefined. Using clienId as claim identity for the access_token - M2M Context with client-credentials");
+    identity_for_access_token = event.callerContext.clientId;
   }
-  console.debug(identity_for_access_token);
+  console.debug("identity=", identity_for_access_token);
   event.response = {
     "claimsAndScopeOverrideDetails": {
       "idTokenGeneration": {
         "claimsToAddOrOverride": {
           "identity": event.request.userAttributes.email
-    }
+        }
       },
       "accessTokenGeneration": {
         "claimsToAddOrOverride": {
@@ -34,6 +28,5 @@ export const handler = function(event, context) {
       },
     }
   };
-  // Return to Amazon Cognito
-  context.done(null, event);
+  return event;
 };
